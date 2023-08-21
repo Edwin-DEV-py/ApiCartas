66a92dd956e9a1586495fb320b9060c70ecbbbf1
edwin-papasito <nicolas.tarazona.morales@gmail.com>
2023-08-20 21:59:51 -0500

    vuista de prueba

--- ApiCartas/Cartas/views.py
+++ ApiCartas/Cartas/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from .models import Card
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from concurrent.futures import ThreadPoolExecutor
+from .serializers import CardSerializer
+import requests
 
-# Create your views here.
+class CardView(APIView):
+    def get(request):
+        cards = Card.objects.all()
+        serializer = CardSerializer(cards,many=True)
+        return Response(serializer.data)
