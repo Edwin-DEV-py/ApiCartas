03e75d280230686b96b53cc82bf74184fcc66efa
edwin-papasito <nicolas.tarazona.morales@gmail.com>
2023-08-20 21:54:24 -0500

    modleo carta

--- ApiCartas/Cartas/models.py
+++ ApiCartas/Cartas/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+class Card(models.Model):
+    id_carta = models.IntegerField()
+    stock = models.IntegerField()
+    price = models.DecimalField(max_digits = 6,decimal_places = 2)
