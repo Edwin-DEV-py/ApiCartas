8366c9f792bebe963073f1064a3f300d43164aa6
edwin-papasito <nicolas.tarazona.morales@gmail.com>
2023-08-20 23:23:48 -0500

    vista de las cartas

--- ApiCartas/Cartas/views.py
+++ ApiCartas/Cartas/views.py
@@ -7,7 +7,34 @@ from .serializers import CardSerializer
 import requests
 
 class CardView(APIView):
-    def get(request):
-        cards = Card.objects.all()
-        serializer = CardSerializer(cards,many=True)
-        return Response(serializer.data)
+    def get(self,request):
+        try:
+            all_cards = Card.objects.all()
+            card_data = []
+            
+            def get_card_data(card_item):
+                id_carta = card_item.id_carta
+                price = card_item.price
+                stock = card_item.stock
+                
+                response = requests.get('') #endpoint del inventario
+                data = response.json()
+                
+                card_item_data = {
+                    'id_carta': id_carta,
+                    'name': data['name'],
+                    'description':data['description'],
+                    'price':price,
+                    'stock': stock
+                }
+                
+                return card_item_data
+            
+            with ThreadPoolExecutor() as executor:
+                card_data = list(executor.map(get_card_data, all_cards))
+                
+            return Response(card_data)
+        
+        except Card.DoesNotExist:
+            return Response({'error':'No hay cartas'}, status=404)
+                
