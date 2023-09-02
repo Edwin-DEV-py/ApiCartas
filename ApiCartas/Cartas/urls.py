3bf4bab430c7b69f6051e01b4a3bfab0a2753b35
edwin-papasito <nicolas.tarazona.morales@gmail.com>
2023-09-01 23:48:14 -0500

    Carta por ID

--- ApiCartas/Cartas/urls.py
+++ ApiCartas/Cartas/urls.py
@@ -3,6 +3,7 @@ from .views import *
 
 urlpatterns = [
     path('cards/',CardView.as_view(),name='Cartas'),
+    path('cardDetail/<str:id_carta>',CardDetail.as_view(),name='Detalle'),
     path('characters/',CardCharacterView.as_view(),name='Personajes'),
     path('guns/',CardGunView.as_view(),name='Personajes'),
     path('armors/',CardArmorView.as_view(),name='Personajes'),
