f5cb7eb2e82e96502810f1efc362fa89c900fc34
edwin-papasito <nicolas.tarazona.morales@gmail.com>
2023-09-09 01:12:21 -0500

    modelo de membresias para pagar

--- ApiCartas/Cartas/models.py
+++ ApiCartas/Cartas/models.py
@@ -7,3 +7,10 @@ class Card(models.Model):
     stock = models.IntegerField()
     price = models.IntegerField()
     is_available = models.BooleanField(default=True)
+    
+class Cardgames(models.Model):
+    id = models.AutoField(primary_key=True)
+    title = models.CharField(max_length=20)
+    games = models.IntegerField()
+    sub = models.BooleanField(default=False)
+    price = models.IntegerField()
