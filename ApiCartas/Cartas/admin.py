d3292011b539aa38d897f42c21bc81fbeab4952c
edwin-papasito <nicolas.tarazona.morales@gmail.com>
2023-09-13 00:15:09 -0500

    membresias creadas

--- ApiCartas/Cartas/admin.py
+++ ApiCartas/Cartas/admin.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
-from .models import Card
+from .models import Card, Cardgames
 
 class CardAdmin(admin.ModelAdmin):
     list_display = ('id_carta','nombre_carta' ,'stock', 'price')
+    
+class CardgamesAdmin(admin.ModelAdmin):
+    list_display = ('id_carta','title' ,'games','sub', 'price')
 
 admin.site.register(Card,CardAdmin)
+admin.site.register(Cardgames,CardgamesAdmin)
--- ApiCartas/Cartas/models.py
+++ ApiCartas/Cartas/models.py
@@ -9,7 +9,7 @@ class Card(models.Model):
     is_available = models.BooleanField(default=True)
     
 class Cardgames(models.Model):
-    id = models.AutoField(primary_key=True)
+    id_carta = models.AutoField(primary_key=True)
     title = models.CharField(max_length=20)
     games = models.IntegerField()
     sub = models.BooleanField(default=False)
