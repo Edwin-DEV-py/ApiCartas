db06e33a3e1835ff42d6a35c7e065814ae6765b0
edwin-tarazona <80715680+Edwin-DEV-py@users.noreply.github.com>
2023-09-13 14:47:14 -0500

    Update urls.py

--- ApiCartas/Cartas/urls.py
+++ ApiCartas/Cartas/urls.py
@@ -3,7 +3,7 @@ from .views import *
 
 urlpatterns = [
     path('cards/',CardView.as_view(),name='Cartas'),
-    path('membership/',CardgamesView.as_view(),name='Membresias'),
+    path('membership/',Membership.as_view(),name='Membresias'),
     path('cards2/',example.as_view(),name='Cartas2'),#prueba
     path('cardDetail/<str:id_carta>',CardDetail.as_view(),name='Detalle'),
     path('characters/',CardCharacterView.as_view(),name='Personajes'),
