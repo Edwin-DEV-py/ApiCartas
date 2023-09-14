60219ae64f6dd3dc75ce4617c31330f2aee51fd0
edwin-papasito <nicolas.tarazona.morales@gmail.com>
2023-09-14 01:26:21 -0500

    ya sirve buscar por tipo, heroe o demas cosas

--- ApiCartas/Cartas/views.py
+++ ApiCartas/Cartas/views.py
@@ -5,6 +5,7 @@ from rest_framework.response import Response
 from concurrent.futures import ThreadPoolExecutor
 from .serializers import CardSerializer, CardgamesSerializer
 from rest_framework import status
+from django.db.models import Q
 import requests
 
 #endpoint de todas las cartas
@@ -18,6 +19,10 @@ class example(APIView):
 class CardView(APIView):
     def get(self, request):
         try:
+            
+            #indexacion y busqueda
+            tipo = request.data.get('tipo')
+            heroe = request.data.get('heroe')
             
             #obtener los datos del inventario
             response = requests.get('https://main-api-cartas.thenexusbattles2.com/api/all/')
@@ -34,30 +39,55 @@ class CardView(APIView):
             page_data = data[start_index:end_index] #aqui mostramos las cartas
 
             card_data = []
+            if tipo is None and heroe is None:
+                #aca mapeamos los datos para unirlos con la inforacion de la DB de precios y stock
+                for carta_data in page_data:
+                    id_carta = carta_data.get('_id', '')
 
-            #aca mapeamos los datos para unirlos con la inforacion de la DB de precios y stock
-            for carta_data in page_data:
-                id_carta = carta_data.get('_id', '')
-                
 
-                card_item_data = {
-                    'id_carta': id_carta,
-                }
+                    card_item_data = {
+                        'id_carta': id_carta,
+                    }
 
-                # Iterar a través de todas las propiedades del objeto
-                for key, value in carta_data.items():
-                    if key != '_id':
-                        card_item_data[key] = value
+                    # Iterar a través de todas las propiedades del objeto
+                    for key, value in carta_data.items():
+                        if key != '_id':
+                            card_item_data[key] = value
 
-                try:
-                    card_db = Card.objects.get(id_carta=id_carta)
-                    card_item_data['price'] = card_db.price
-                    card_item_data['stock'] = card_db.stock
-                    card_item_data['nombre_carta'] = card_db.nombre_carta
-                except Card.DoesNotExist:
-                    pass  # No se encontró la carta
+                    try:
+                        card_db = Card.objects.get(id_carta=id_carta)
+                        card_item_data['price'] = card_db.price
+                        card_item_data['stock'] = card_db.stock
+                        card_item_data['nombre_carta'] = card_db.nombre_carta
+                    except Card.DoesNotExist:
+                        pass  # No se encontró la carta
 
-                card_data.append(card_item_data)
+                    card_data.append(card_item_data)
+            
+            else:
+                #iterar por el json
+                for carta in data:
+                    #comparar el id del json con el id que pasamos
+                    if carta.get('tipo', '').lower() == tipo.lower():
+                        carta_data = {
+                            'id_carta':carta.get('_id',''),
+
+                        }
+                        # Iterar a través de todas las propiedades del objeto
+                        for key, value in carta.items():
+                            if key != '_id':
+                                carta_data[key] = value
+
+                        #ahora incorporamos los datos de la DB local
+                        try:
+                            card_db = Card.objects.get(id_carta=carta_data['id_carta'])
+                            carta_data['price'] = card_db.price
+                            carta_data['stock'] = card_db.stock
+                            carta_data['nombre_carta'] = card_db.nombre_carta
+                        except Card.DoesNotExist:
+                            pass  # No se encontró la carta
+                        
+                        card_data.append(carta_data)
 
             return Response(card_data)
 
@@ -80,7 +110,7 @@ class CardDetail(APIView):
                 #comparar el id del json con el id que pasamos
                 if carta.get('_id') == id_carta:
                     card_data = carta
-                    break
+                    pass
             
             #ahora incorporamos los datos de la DB local
             try:
